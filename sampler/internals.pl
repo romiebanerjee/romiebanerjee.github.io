52c41f616320f93d88e2337e0c6aa920de5a21ca
Romie Banerjee <romie.banerjee@gmail.com>
2025-08-04 12:09:03 +0200

    sampler html files part 2

---
+++ sampler/internals.pl
@@ -0,0 +1,50 @@
+# LaTeX2HTML 2024 (Released January 1, 2024)
+# Associate internals original text with physical files.
+
+
+$key = q/cite_gal2015bayesian/;
+$ref_files{$key} = "$dir".q|sampler.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_gal2016dropout/;
+$ref_files{$key} = "$dir".q|sampler.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_glm/;
+$ref_files{$key} = "$dir".q|sampler.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_kfac/;
+$ref_files{$key} = "$dir".q|sampler.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_kfac-cnn/;
+$ref_files{$key} = "$dir".q|sampler.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_mackay/;
+$ref_files{$key} = "$dir".q|sampler.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_sinf/;
+$ref_files{$key} = "$dir".q|sampler.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/glm_unc/;
+$ref_files{$key} = "$dir".q|sampler.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/glm_unc_kfac/;
+$ref_files{$key} = "$dir".q|sampler.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/matrix/;
+$ref_files{$key} = "$dir".q|sampler.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sigma_kfac/;
+$ref_files{$key} = "$dir".q|sampler.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
